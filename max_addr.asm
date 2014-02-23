2b60b26c7d7c64e1403f63e0cc71762e8f009eca
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2014-02-22 21:11:45 -0400

    init

---
+++ max_addr.asm
@@ -0,0 +1,52 @@
+#PURPOSE: This program finds the maximum number of a
+#         set of data items.
+#
+
+#VARIABLES: The registers have the following uses:
+#
+#           %edi - Holds the index of the data item being examined
+#           %eax - Current data item
+#           %ebx - Largest data item found
+#
+#           The following memory locations are used:
+#
+#           data_items - Contains the item data
+#           end        - Contains the number of item
+#           end_addr   - Contains the end address
+
+.section .data
+data_items:
+	.long 3,67,34,222,45,75,54,43,44,33,22,11,66
+end:
+	.long 12
+end_addr:
+	.long 0
+
+.section .text
+
+.globl _start
+_start:
+	movl end, %ecx
+	leal data_items(,%ecx,4), %ecx   # compute end address
+	movl %ecx, end_addr              # move end address to end_addr
+	movl $0, %edi                    # move 0 into the index register
+	movl data_items(,%edi,4), %eax   # load the first byte of data
+	movl %eax, %ebx                  # since this is the first item, %eax
+                                         # is the biggest
+
+start_loop:
+	leal data_items(,%edi,4), %ecx   # compute current address
+	cmpl %ecx, end_addr              # compare end and current address
+	je loop_exit                     # jump to loop_exit
+	incl %edi                        # increment %edi
+	movl data_items(,%edi,4), %eax   # load next value in %eax
+	cmpl %ebx, %eax                  # compare values
+	jle start_loop                   # jump to loop beginning if the new
+                                         # one isn't bigger
+	movl %eax, %ebx                  # move the value as the largest
+	jmp start_loop                   # jump to loop beginning
+	
+loop_exit:
+	movl $1, %eax                    # 1 is the exit() syscall
+                                         # %ebx is loaded with the max value
+	int $0x80                        # linux kernel syscall
